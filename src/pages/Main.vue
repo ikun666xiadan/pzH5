71673228ab291c8f344976496131b44d47bb0bf4
Y <2893620464@qq.com>
2025-07-31 09:32:02 +0800

    feat：实现底部标签栏和安装less

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
     <RouterView/>
+    <van-tabbar v-model="active">
+      <van-tabbar-item v-for="item in tabBarData.children" :key="item.path" :icon="item.meta.icon" :url="`/#${item.path}`">{{item.meta.name}}</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+
+const active = ref(0);
+const router = useRouter()
+const route = useRoute()
+const tabBarData = ref({})
+onMounted(()=>{
+  tabBarData.value = router.options.routes[0]
+  active.value = tabBarData.value.children.findIndex(item => '/' + item.path === route.path)
+})
 
 </script>
 
